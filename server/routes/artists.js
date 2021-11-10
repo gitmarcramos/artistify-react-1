37d29606bfaac8685bf056902f6340dc149ff447
Marc Ramos <marcramos.webdev@gmail.com>
2021-11-10 18:13:53 +0100

    Artist crud WIP

--- server/routes/artists.js
+++ server/routes/artists.js
@@ -5,17 +5,18 @@ const express = require("express");
 const router = new express.Router();
 
 const artistModel = require("../model/Artist");
- const styleModel = require("../model/Style");
+const styleModel = require("../model/Style");
 
 //CREATE the artists
 router.post("/", async (req, res, next) => {
   try {
+    console.log(req.body)
     // get const variables from the req.body
     const { name, description, isBand, styleName, color, wikiURL } = req.body;
     // get the variable style from the select input (req.body)
-    let {style}  = req.body;
+    let { style } = req.body;
 
-    // Check if there is values to create a new style or use existing values from the select input 
+    // Check if there is values to create a new style or use existing values from the select input
     if (!styleName && !wikiURL) {
       const newStyle = await styleModel.create({
         styleName,
@@ -27,7 +28,12 @@ router.post("/", async (req, res, next) => {
     }
 
     //create the new artist with values from the inputs
-    const newArtist = await artistModel.create({name, description, isBand, style});
+    const newArtist = await artistModel.create({
+      name,
+      description,
+      isBand,
+      style,
+    });
 
     res.status(200).json(newArtist);
   } catch (err) {
@@ -60,8 +66,11 @@ router.get("/:id", async (req, res) => {
 //UPDATE
 router.patch("/:id", async (req, res, next) => {
   try {
-    const foundArtistandUpdate = await artistModel
-      .findByIdAndUpdate(req.params.id, req.body, { new: true })
+    const foundArtistandUpdate = await artistModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
     res.status(200).json(foundArtistandUpdate);
   } catch (err) {
     console.log(error);
